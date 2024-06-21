5d0af716d031bb30525ac77c7acd6d4f80bf0edf
imn00ne <rkmanishtiwari@gmail.com>
2024-06-21 15:25:56 +0530

    basic structur

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config({path:"./.env"});
+const path = require("path");
+const express = require("express");
+const app = express();
+
+
+// connect to database
+require("./models/database").connectDB();
+
+const userRoutes = require("./routes/routes.user");
+
+// configure views
+app.set("view engine", "ejs");
+`app.set("views", path.join(__dirname, "views"));`
+app.use("/user", userRoutes);
+
+// server
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running`);
+});
